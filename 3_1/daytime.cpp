0cb7c3c174e05334fa688fe369c99fdf4db2c5c9
Marcus-RR <zybziba@yandex.ru>
2023-12-17 18:25:07 +0300

    Commit 3

---
+++ 3_1/daytime.cpp
@@ -0,0 +1,43 @@
+
+#include <bits/stdc++.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <netinet/in.h> 
+   
+#define PORT     8080 
+#define MAXLINE 1024 
+   
+int main() { 
+    int sockfd; 
+    char buffer[MAXLINE]; 
+    struct sockaddr_in     servaddr; 
+   
+    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
+        perror("socket creation failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+   
+    memset(&servaddr, 0, sizeof(servaddr)); 
+       
+    servaddr.sin_family = AF_INET; 
+    servaddr.sin_port = htons(PORT); 
+    servaddr.sin_addr.s_addr = INADDR_ANY; 
+       
+    int n;
+    socklen_t len; 
+    sendto(sockfd, nullptr, 0, 0, (struct sockaddr*) &servaddr, sizeof(servaddr)); 
+    std::cout<<"Empty package sent."<<std::endl; 
+           
+    n = recvfrom(sockfd, (char *)buffer, MAXLINE,  
+                MSG_WAITALL, (struct sockaddr *) &servaddr, 
+                &len); 
+    buffer[n] = '\0'; 
+    std::cout<<"Server :"<<buffer<<std::endl; 
+   
+    close(sockfd); 
+    return 0; 
+}
